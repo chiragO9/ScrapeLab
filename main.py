6e101c06caafd1df13857cbd9a92cc483c72391e
Chirag Solanki <solankichiragcs12@gmail.com>
2025-12-27 15:13:56 +0530

    add all books category

--- main.py
+++ main.py
@@ -9,17 +9,25 @@ books = soup.find_all('li', class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
 print(f"Total Books Found: {len(books)}\n")
 print("=" * 70)
 
+book_categories = soup.find('div', class_ = 'side_categories')
+categories = book_categories.find('ul').find_all('a')
+for index, category in enumerate(categories, 1):
+    category_name = category.text.strip()
+    print(f"{index}. {category_name}")
+
+print("\n")
+
 for index, book in enumerate(books, 1):
     
     book_title = book.find('h3').a['title']
 
-    # Get price
+    
     price = book.find('p', class_='price_color').text
     
     
     rating = book.find('p', class_='star-rating')['class'][1]
     
-    # Print using f-string
+    
     print(f"{index}. Title: {book_title}")
     print(f"   Price: {price}")
     print(f"   Rating: {rating} stars")
